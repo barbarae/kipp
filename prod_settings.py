ed9a82cfb3e3b03d59313aa224d2c1e7bf196047
kipp <kipp@librarian.(none)>
2010-08-22 20:27:13 +0000

    prod settings

---
+++ prod_settings.py
@@ -0,0 +1,15 @@
+from common_settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'kipp',                      # Or path to database file if using sqlite3.
+        'USER': 'kipp',                      # Not used with sqlite3.
+        'PASSWORD': 'd3s1gnanew',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
