6a656505bb5d5c3329277c4d1f0818ca297b6b0c
meirish <matthew.irish@gmail.com>
2010-08-22 15:31:13 -0500

    changed gitignore

--- filer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-django-filer - file and image management for django projects
-"""
-VERSION = (0,6,2,'a5')
-__version__ = "0.6.2a5"
-__authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
--- filer/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from filer.models.foldermodels import *
-from filer.models.filemodels import * 
-from filer.models.imagemodels import *
-from filer.models.clipboardmodels import *
-from filer.models.virtualitems import *
-from filer.models.favoritemodels import *
